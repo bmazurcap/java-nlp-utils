f2ccdff40fea5d5845f1dcab1f2400e18333fae3
bartmazu <bartlomiej.mazur@capgemini.com>
2025-01-10 16:08:53 +0100

    Init-to-java-8-javac

---
+++ build.bat
@@ -0,0 +1,2 @@
+dir /s /B *.java > C:/Users/bartmazu/AppData/Local/Temp/java-nlp-utils/src/sources.txt
+"C:\Users\bartmazu\Downloads\jdk8u432-b06\bin\javac.exe" -cp "C:\Users\bartmazu\Downloads\junit-4.8.1.jar" -extdirs jars @C:/Users/bartmazu/AppData/Local/Temp/java-nlp-utils/src/sources.txt -d "C:/Users/bartmazu/AppData/Local/Temp/java-nlp-utils/src/classes"
